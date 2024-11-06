d079d522d2ab7ec7913f5b152f1d81228c9249b7
YaguarEgor <Egor072005@yandex.ru>
2024-11-06 22:10:17 +0300

    shell 2.0 is ready

--- prac4/shell.c
+++ prac4/shell.c
@@ -33,36 +33,21 @@ int main(int argc, char** argv) {
     return 0;
 }
 
-int executeCommands(char*** commands, int* number_of_commands) {
-    (*commands) = (char**)realloc((*commands), (*number_of_commands+1)*sizeof(char*));
-    (*commands)[*number_of_commands] = (char*)0;
-    pid_t pid;
-    if ((pid = fork()) < 0) {
-        fprintf(stderr, "Error");
-        return -1;
-    }
-    else if (!pid) {
-        if (strcmp((*commands)[0], "cd") == 0) {
-            if (*number_of_commands > 1) {
-                if (chdir((*commands)[1]) != 0)
-                    perror("Error: Failed to change directory(1)\n");
-            }
-            else {
-                if (chdir(getenv("HOME")) != 0) {
-                    perror("Error: Failed to change directory to parent(1)\n");
-                }
-            }
-            return 0;
+
+void getAllCommands(FILE* inp, char*** mas_of_words, int* number_of_words) {
+    int code = 0;
+    while (code != END) {
+        (*number_of_words) = getWordsFromLine(inp, mas_of_words, number_of_words, &code);
+        if ((*mas_of_words)[0]) {
+            executeCommands(mas_of_words, number_of_words);
         }
-        else {
-            execvp((*commands)[0], *commands);
-            perror("Error: Failed to find such command(2)\n");
-            return -1;
-        }
+        freeMemory(*mas_of_words, *number_of_words + 1);
+        *mas_of_words = NULL;
+        (*number_of_words) = 0;
+        fprintf(stdout, "\n");
     }
-    wait(NULL);
-    return 0;
 }
+
 
 int getWordsFromLine(FILE* inp, char*** mas_of_words, int* number_of_words, int* code) {
     int special_symbol_is_used = 0;
@@ -98,7 +83,7 @@ int getWordsFromLine(FILE* inp, char*** mas_of_words, int* number_of_words, int*
                 }
                 else {
                     free(word);
-                    return n;
+                    return n-1;
                 }
             }
             
@@ -164,21 +149,41 @@ int getWordsFromLine(FILE* inp, char*** mas_of_words, int* number_of_words, int*
     return -1;
 }
 
+
+int executeCommands(char*** commands, int* number_of_commands) {
+    (*commands) = (char**)realloc((*commands), (*number_of_commands+1)*sizeof(char*));
+    (*commands)[*number_of_commands] = (char*)0;
+    if (strcmp((*commands)[0], "cd") == 0) {
+        if (*number_of_commands > 1) {
+            if (chdir((*commands)[1]) != 0)
+                perror("Error: Failed to change directory(1)\n");
+        }
+        else {
+            if (chdir(getenv("HOME")) != 0) {
+                perror("Error: Failed to change directory to parent(1)\n");
+            }
+        }
+    }
+    else {
+        pid_t pid;
+        if ((pid = fork()) < 0) {
+            fprintf(stderr, "Error");
+            return -1;
+        }
+        else if (!pid) {
+            execvp((*commands)[0], *commands);
+            perror("Error: Failed to find such command(2)\n");
+            return -1;
+        }
+        wait(NULL);
+    }
+    return 0;
+}
+
+
 void freeMemory(char** mas_words, int len) {
     for (int i = 0; i < len; i++) {
         free(mas_words[i]);
     }
     free(mas_words);
-}
-
-void getAllCommands(FILE* inp, char*** mas_of_words, int* number_of_words) {
-    int code = 0;
-    while (code != END) {
-        (*number_of_words) = getWordsFromLine(inp, mas_of_words, number_of_words, &code);
-        executeCommands(mas_of_words, number_of_words);
-        freeMemory(*mas_of_words, *number_of_words);
-        *mas_of_words = NULL;
-        (*number_of_words) = 0;
-        fprintf(stdout, "\n");
-    }
 }
