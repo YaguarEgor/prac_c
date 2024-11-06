85338827a4b067869561204904ca30b4bb7804f1
YaguarEgor <Egor072005@yandex.ru>
2024-11-06 14:30:37 +0300

    Prac4 part2 is ready:

--- prac4/shell.c
+++ prac4/shell.c
@@ -3,11 +3,66 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <errno.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
 #define NL 1
 #define END 2
 #define SPECIALSYMBOLS "&|;><()"
 
+int executeCommands(char*** commands, int* number_of_commands);
+
+int getWordsFromLine(FILE* inp, char*** mas_of_words, int* number_of_words, int* code);
+
+void freeMemory(char** mas_words, int len);
+
+void getAllCommands(FILE* inp, char*** mas_of_words, int* number_of_words);
+
+int main(int argc, char** argv) {
+    FILE* input = NULL;
+    if (argc > 1) {
+        input = fopen(argv[1], "r");
+    }
+    input = input ? input : stdin;
+    
+    int len = 0;
+    char** mas_of_words = NULL;
+    getAllCommands(input, &mas_of_words, &len);
+    
+    return 0;
+}
+
+int executeCommands(char*** commands, int* number_of_commands) {
+    (*commands) = (char**)realloc((*commands), (*number_of_commands+1)*sizeof(char*));
+    (*commands)[*number_of_commands] = (char*)0;
+    pid_t pid;
+    if ((pid = fork()) < 0) {
+        fprintf(stderr, "Error");
+        return -1;
+    }
+    else if (!pid) {
+        if (strcmp((*commands)[0], "cd") == 0) {
+            if (*number_of_commands > 1) {
+                if (chdir((*commands)[1]) != 0)
+                    perror("Error: Failed to change directory(1)\n");
+            }
+            else {
+                if (chdir(getenv("HOME")) != 0) {
+                    perror("Error: Failed to change directory to parent(1)\n");
+                }
+            }
+            return 0;
+        }
+        else {
+            execvp((*commands)[0], *commands);
+            perror("Error: Failed to find such command(2)\n");
+            return -1;
+        }
+    }
+    wait(NULL);
+    return 0;
+}
 
 int getWordsFromLine(FILE* inp, char*** mas_of_words, int* number_of_words, int* code) {
     int special_symbol_is_used = 0;
@@ -116,34 +171,14 @@ void freeMemory(char** mas_words, int len) {
     free(mas_words);
 }
 
-void getAllWords(FILE* inp, char*** mas_of_words, int* number_of_words) {
+void getAllCommands(FILE* inp, char*** mas_of_words, int* number_of_words) {
     int code = 0;
     while (code != END) {
         (*number_of_words) = getWordsFromLine(inp, mas_of_words, number_of_words, &code);
-        if (inp == stdin)
-            fprintf(stdout, "\n");
-        for (int i = 0; i < (*number_of_words); i++) {
-            if ((*mas_of_words)[i] != NULL)
-                fprintf(stdout, "%s\n", (*mas_of_words)[i]); 
-        }
+        executeCommands(mas_of_words, number_of_words);
         freeMemory(*mas_of_words, *number_of_words);
         *mas_of_words = NULL;
         (*number_of_words) = 0;
         fprintf(stdout, "\n");
     }
-}
-
-
-int main(int argc, char** argv) {
-    FILE* input = NULL;
-    if (argc > 1) {
-        input = fopen(argv[1], "r");
-    }
-    input = input ? input : stdin;
-    
-    int len = 0;
-    char** mas_of_words = NULL;
-    getAllWords(input, &mas_of_words, &len);
-    
-    return 0;
 }
