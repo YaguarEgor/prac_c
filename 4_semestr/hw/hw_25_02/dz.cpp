f5ba848c61c8afab965d6c3e4cc29032e5eeac6b
YaguarEgor <Egor072005@yandex.ru>
2025-02-23 18:14:27 +0300

    HW on 25.02 added

--- 4_semestr/hw/hw_25_02/dz.cpp
+++ 4_semestr/hw/hw_25_02/dz.cpp
@@ -1,175 +1,204 @@
 #include <iostream>
-#include <cstring>
+#include <cmath>
+#include <iomanip>
 
-using namespace std;
+class DateTimeSpan;
 
-class Book {
-    // char* name;
-    // char* author;
-    string name;
-    string author;
-    int published_at;
-public:
-    Book() {};
+class DateTime {
+    double m_value;
 
-    // Book(const char* name, const char* author, int published_at) {
-    //     this -> name = new char[strlen(name)+1];
-    //     strcpy(this->name, name);
-    //     this -> author = new char[strlen(author)+1];
-    //     strcpy(this->author, author);
-    //     this -> published_at = published_at;
-    // };
-
-    Book(const string name, const string author, int published_at) {
-        this -> name = name;
-        this -> author = author;
-        this -> published_at = published_at;
-    };
-
-    string get_name() const{
-        return this -> name;       
+    bool isLeapYear(int year) const {
+        return ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0));
     }
-    string get_author() const {
-        return this -> author;
+    
+    int getDaysInMonth(int year, int month) const {
+        int daysInMonths[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        if (month == 2 && isLeapYear(year))
+            return 29;
+        return daysInMonths[month-1];
     }
-    int get_published_at() const {
-        return this -> published_at;
+    
+    void computeDate(int offset, int &year, int &month, int &day) const {
+        year = 2000;
+        month = 1;
+        day = 1;
+        
+        if (offset > 0) {
+            while (offset > 0) {
+                int daysInCurrentMonth = getDaysInMonth(year, month);
+                int remaining = daysInCurrentMonth - day;
+                if (offset <= remaining) {
+                    day += offset;
+                    offset = 0;
+                } else {
+                    offset -= (remaining + 1);
+                    day = 1;
+                    month++;
+                    if (month > 12) {
+                        month = 1;
+                        year++;
+                    }
+                }
+            }
+        } else if (offset < 0) {
+            offset = -offset;
+            while (offset > 0) {
+                if (offset < day) {
+                    day -= offset;
+                    offset = 0;
+                } else {
+                    offset -= day;
+                    month--;
+                    if (month < 1) {
+                        month = 12;
+                        year--;
+                    }
+                    day = getDaysInMonth(year, month);
+                }
+            }
+        }
     }
-};
-
-class HomeLibrary {
-    int size_of_library;
-    Book* books;
     
 public:
-    HomeLibrary(): size_of_library(0), books(nullptr) {};
+    DateTime() : m_value(0.0) { }
+    
+    DateTime(double value) : m_value(value) { }
 
-    void add_book(string name, string author, int published_at) {
-        Book* new_books = new Book[size_of_library+1];
-        for (int i = 0; i < size_of_library; i++) {
-            new_books[i] = books[i];
-        }
-        new_books[size_of_library++] = Book(name, author, published_at);
-        delete[] books;
-        books = new_books;
+    void print_date() const {
+        int daysOffset = std::floor(m_value);
+        double fraction = m_value - std::floor(m_value);
+        
+        int year, month, day;
+        computeDate(daysOffset, year, month, day);
+        
+        int totalSeconds = std::round(fraction * 24*3600);
+
+        int hour = totalSeconds / 3600;
+        int minute = (totalSeconds % 3600) / 60;
+        int second = totalSeconds % 60;
+        
+        std::cout << std::setfill('0')
+           << std::setw(2) << day << "."
+           << std::setw(2) << month << "."
+           << std::setw(4) << year << " "
+           << std::setw(2) << hour << ":"
+           << std::setw(2) << minute << ":"
+           << std::setw(2) << second << std::endl;
     }
 
-    void get_book_by_author(string value) {
-        if (this->size_of_library == 0) {
-            cout << "В Вашей библиотеке пока нет книг" << endl;
-            return;
-        }
-        cout << "Все книги, автора которых зовут " << value << ":" << endl;
-        for (int i = 0; i < this->size_of_library; i++) {
-            if (this->books[i].get_author() == value) {
-                cout << "Книга '" << this->books[i].get_name() << "', год выхода: " << this->books[i].get_published_at() << endl;
-            }
-        }
+    double get_m() const{
+        return m_value;
+    }
+    
+    DateTimeSpan operator-(const DateTime& other);
+
+    DateTime operator+ (const DateTimeSpan& span);
+
+    DateTime operator- (const DateTimeSpan& span);
+};
+
+class DateTimeSpan {
+    double m_value;
+
+public:
+    DateTimeSpan (int days = 0, int hours = 0, int minutes = 0, int seconds = 0) {
+        m_value = double(seconds)*(1.0/3600*24) + double(minutes)*(1.0/24*60) + double(hours)*(1.0/24) + days;
     }
 
-    void get_book_by_name(string value) {
-        if (this->size_of_library == 0) {
-            cout << "В Вашей библиотеке пока нет книг" << endl;
-            return;
-        }
-        cout << "Все книги, которые назваются " << value << ":" << endl;
-        for (int i = 0; i < this->size_of_library; i++) {
-            if (this->books[i].get_author() == value) {
-                cout << "Книга автора " << this->books[i].get_author() << ", год выхода: " << this->books[i].get_published_at() << endl;
-            }
-        }
+    DateTimeSpan (double span) {
+        m_value = span;
+    } 
+
+    double get_diff() const {
+        return m_value;
     }
 
-    void get_book_by_published_at(int value) {
-        if (this->size_of_library == 0) {
-            cout << "В Вашей библиотеке пока нет книг" << endl;
-            return;
-        }
-        cout << "Все книги, которые были выпущены в " << value << " году:" << endl;
-        for (int i = 0; i < this->size_of_library; i++) {
-            if (this->books[i].get_published_at() == value) {
-                cout << "Книга '" << this->books[i].get_name() << "', автор: " << this->books[i].get_author() << endl;
-            }
-        }
+    DateTimeSpan operator+(const DateTimeSpan &other) const {
+        return DateTimeSpan(m_value + other.m_value);
+    }
+    
+    DateTimeSpan operator-(const DateTimeSpan &other) const {
+        return DateTimeSpan(m_value - other.m_value);
     }
 
-    void get_all_books() {
-        if (this->size_of_library == 0) {
-            cout << "В Вашей библиотеке пока нет книг" << endl;
-            return;
-        }
-        for (int i = 0; i < this->size_of_library; ++i) {
-            cout << "Книга №" << i+1 << ":" << endl << "  Название: " << this->books[i].get_name() << endl << "  " 
-            "Автор: " << this->books[i].get_author() << endl << "  Год выпуска: " << this->books[i].get_published_at() << endl;
-        }
+    DateTimeSpan operator-() const {
+        return DateTimeSpan(-m_value);
     }
 
-    void get_commands() {
-        cout << "Добро пожаловать в Домашнюю библиотеку! Сейчас она у вас пустая,"
-        " но Вы можете это исправить. Итак, давайте перейдем к командам, чтобы менять библиотеку:" << endl << " "
-        "0) Вывести все книги в Вашей библиотеке" << endl << " "
-        "1) Добавить книгу. Вас попросят ввести название и автора книги, год публикации необязателен (по умолчанию - 2025)" << endl << " "
-        "2) Поиск книги по автору. Введите автора и получите все его книги в Вашей библиотеке" << endl << " "
-        "3) Поиск книги по названию. Ввведите название и получите всн книги с таким названием" << endl << " "
-        "4) Поиск книги по дате публикации. Введите дату и получите все книги этого года выпуска в Вашей библиотеке" << endl << ""
-        "5) Выйти из библиотеки. Завершит взаимодействие с Домашней библиотекой" << endl << endl << ""
-        "Чтобы ввести одну их этих команд, напишите номер команды (без закрывающей скобки) и следуйте дальнейшим подсказкам" << endl;
-    }
-
-    ~HomeLibrary() {
-        delete[] this->books;
+    void print() const {
+        int days = std::floor(m_value);
+        int totalSeconds = (m_value - days)*3600*24;
+        int hour = totalSeconds / 3600;
+        int minute = (totalSeconds % 3600) / 60;
+        int second = totalSeconds % 60;
+        std::cout << "Days: " << days << ", hours: " << hour << ", minutes: " << minute << ", seconds: " << second << std::endl;
     }
 };
 
+DateTimeSpan DateTime::operator-(const DateTime& other) {
+    return DateTimeSpan(this->m_value - other.get_m());
+}
+
+DateTime DateTime::operator+ (const DateTimeSpan& span) {
+    return DateTime(this->m_value + span.get_diff());
+}
+
+DateTime DateTime::operator- (const DateTimeSpan& span) {
+    return DateTime(this->m_value - span.get_diff());
+}
+
 int main() {
-    int num_cmd;
-    string title, author;
-    int date;
-    HomeLibrary library;
-    library.get_commands();
-    while (1) {
-        cout << "Введите номер команды: ";
-        cin >> num_cmd;
-        switch (num_cmd) {
-            case 0:
-                library.get_all_books();
-                break;
-            case 1:
-                cout << "Как называется новая книга? ";
-                cin.ignore();
-                getline(cin, title);
-                cout << "Как зовут ее автора? ";
-                getline(cin, author);
-                cout << "Год публикации? ";
-                cin >> date;
-                if (date > 2025) {
-                    date = 2025;
-                }
-                library.add_book(title, author, date);
-                break;
-            case 2:
-                cout << "Введите имя автора, книги которого будем искать: ";
-                cin.ignore();
-                getline(cin, author);
-                library.get_book_by_author(author);
-                break;
-            case 3:
-                cout << "Введите название книги, которую будем искать: ";
-                cin.ignore();
-                getline(cin, title);
-                library.get_book_by_name(title);
-                break;
-            case 4:
-                cout << "Введите год публикации книги, по которому будем искать: ";
-                cin >> date;
-                library.get_book_by_published_at(date);
-                break;
-            case 5:
-                return 0;
-            default:
-                cout << "Такой команды нет" << endl;
-                break;
-        }
-        
-    }
+    DateTime dt1(1.0);       
+    DateTime dt2(2.1);      
+    DateTime dt3(1.75);     
+    DateTime dt4(-1.0);     
+    DateTime dt5(-1.5);     
+    DateTime dt6(-1.25);    
+    DateTime dt7(-1.1);     
+
+    std::cout << "1.0      => ";
+    dt1.print_date();
+    std::cout << "2.1      => ";
+    dt2.print_date();
+    std::cout << "1.75     => ";
+    dt3.print_date();
+    std::cout << "-1.0     => ";
+    dt4.print_date();
+    std::cout << "-1.5     => ";
+    dt5.print_date();
+    std::cout << "-1.25    => ";
+    dt6.print_date();
+    std::cout << "-1.1     => ";
+    dt7.print_date();
+
+    DateTime d1(1.0);      
+    DateTime d2(2.1);      
+    
+    DateTimeSpan s = d2 - d1;
+    std::cout << "d2 - d1 = s = ";
+    s.print();
+    
+    DateTime d3 = d1 + s;
+    std::cout << "d1 + s = ";
+    d3.print_date();
+    
+    DateTime d4 = d2 - s;
+    std::cout << "d2 - s = ";
+    d4.print_date();
+    
+    DateTimeSpan s_neg = -s;
+    std::cout << "-s = ";
+    s_neg.print();
+    
+    DateTimeSpan s1(0, 1, 30, 0);
+    
+    DateTimeSpan s2 = s - s1;
+    std::cout << "s - s1 = ";
+    s2.print();
+    
+    DateTimeSpan s3 = s + s1;
+    std::cout << "s + s1 = ";
+    s3.print();
+
+    return 0;
 }
